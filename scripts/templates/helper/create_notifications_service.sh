d80bb6552b05c7d5148c19c99f039cadc394e1c0
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-06-10 22:05:55 +0300

    feat(notifications): Update NotificationService to improve initialization and badge handling

--- scripts/templates/helper/create_notifications_service.sh
+++ scripts/templates/helper/create_notifications_service.sh
@@ -5,31 +5,43 @@ function create_notifications_service(){
  NOTIFICATIONS_SERVICE_FILE="$DEST_DIR/notifications_service.dart"
   cat <<EOL > "$NOTIFICATIONS_SERVICE_FILE"
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class NotificationService {
   static final _notificationPlugin = FlutterLocalNotificationsPlugin();
-  static final bool _initialized = false;
+  static bool _initialized = false; // Removed 'final' keyword
   static int _badgeCounter = 0;
+
   bool get isInitialized => _initialized;
 
   static Future<void> initNotifications() async {
     if (_initialized) return;
 
-    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
-    const DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings(
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
       requestAlertPermission: true,
       requestBadgePermission: true,
       requestSoundPermission: true,
     );
-    const InitializationSettings initializationSettings = InitializationSettings(
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
       android: initializationSettingsAndroid,
       iOS: initializationSettingsIOS,
     );
+
     await _notificationPlugin.initialize(initializationSettings);
+    _initialized = true; // Set to true after successful initialization
   }
 
-  static NotificationDetails _notificationDetails(){
+  static NotificationDetails _notificationDetails() {
     return NotificationDetails(
-      android: AndroidNotificationDetails('daily_channel_id','Daily Notifications' , channelDescription: 'Daily notifications for the app',
+      android: AndroidNotificationDetails(
+        'daily_channel_id',
+        'Daily Notifications',
+        channelDescription: 'Daily notifications for the app',
         importance: Importance.max,
         priority: Priority.high,
         playSound: true,
@@ -44,11 +56,29 @@ class NotificationService {
         presentSound: true,
         badgeNumber: ++_badgeCounter,
       ),
-
     );
   }
 
-  static Future<void> showNotification({int id = 0, String? title, String? body}) async {
+  static Future<void> resetBadge() async {
+    _badgeCounter = 0;
+    await _notificationPlugin.show(
+      0,
+      null,
+      null,
+      NotificationDetails(
+        iOS: DarwinNotificationDetails(
+          badgeNumber: 0,
+          presentAlert: false,
+          presentSound: false,
+          presentBadge: true,
+        ),
+      ),
+    );
+    await _notificationPlugin.cancelAll();
+  }
+
+  static Future<void> showNotification(
+      {int id = 0, String? title, String? body}) async {
     await _notificationPlugin.show(
       id,
       title,
@@ -57,7 +87,6 @@ class NotificationService {
     );
   }
 }
-
 EOL
 
  echo "📄 Created notifications_service.dart file successfully at $NOTIFICATIONS_SERVICE_FILE"
